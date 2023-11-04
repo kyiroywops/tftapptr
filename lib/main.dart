8f1c80769eff2b22f9907f8e4f170cd6f4b484e9
Kylian Leroy <aleexisazul@gmail.com>
2023-11-04 17:56:35 -0300

    base solo falta colocar providerscope

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tftapp/config/router/app_router.dart';
 import 'package:tftapp/config/theme/app_theme.dart';
 
-void main() {
+
+// faltaria solo colocar el provider con providerscope
+Future<void> main() async{
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
@@ -11,8 +17,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Tft Sinergy',
+      debugShowCheckedModeBanner: false,
+      routerConfig: appRouter,
       theme: AppTheme().themeData,
     );
   }
