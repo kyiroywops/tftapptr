7b478441ed64e12124913b818a109cf72760a506
Kylian Leroy <aleexisazul@gmail.com>
2023-11-09 23:20:07 -0300

    repository

--- lib/presentation/screens/proplayers/home_screen_proplayers.dart
+++ lib/presentation/screens/proplayers/home_screen_proplayers.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tftapp/presentation/providers.dart';
+import 'package:tftapp/presentation/providers/matchs_providers.dart'; // Importa los proveedores
 
-class HomeScreenProPlayers extends StatelessWidget {
+class HomeScreenProPlayers extends ConsumerWidget {
   static const name = 'home-screen-pro-players';
-  const HomeScreenProPlayers({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, ScopedReader watch) {
+    // Lee el repositorio y otros proveedores necesarios
+    final tftMatchRepository = context.read(tftMatchRepositoryProvider);
+    // Otros proveedores aquí si los necesitas
+
+    // Ahora puedes usar el repositorio para obtener datos
+    final matchIds = tftMatchRepository.getMatchIdsByPUUID('yourPUUID', 'yourRegion');
+    // Hacer algo con los matchIds, por ejemplo, cargar el historial
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Historial de partidas'),
+      ),
+      body: Center(
+        child: Text('Contenido de la pantalla de historial aquí'),
+      ),
+    );
   }
 }
